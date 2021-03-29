63a4edd9848cedaf8f8b9b4ad2c8449f0fe0c4ae
unknown <ayvazpetrosyan@gmail.com>
2021-03-30 01:32:09 +0400

    Add user create functionality

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,19 +32,53 @@ class UsersController extends AbstractController
      */
     public function addUser(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('firstNam', 'test')
-            ->add('middleName', 'text')
-            ->add('lastName', 'text')
-            ->add('email', 'email')
-            ->add('userName', 'text')
-            ->add('password','password')
-            ->add('reaped password', 'password')
-            ->add('status','boolean')
-            ->getForm();
+        if ($request->request->get('saveUser') == 'Save') {
+            $user = new User();
+            $user->setFirstName($request->request->get('firstName'));
+            $user->setMiddleName($request->request->get('middleName'));
+            $user->setLastName($request->request->get('lastName'));
+            $user->setUserName($request->request->get('userName'));
+            $user->setPassword($request->request->get('password'));
+            $user->setEmail($request->request->get('email'));
+            $user->setRollId($request->request->get('rollId'));
+            $user->setStatus($request->request->get('status')? $request->request->get('status') : 0);
+            $this->getDoctrine()->getManager()->persist($user);
+            $this->getDoctrine()->getManager()->flush();
+        }
 
-        return $this->render('admin/users/index.html.twig', [
-            'form' => $form->createView()
+        /*$form = $this->createFormBuilder()
+            ->add('firstNam', TextType::class)
+            ->add('middleName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('userName', TextType::class)
+            ->add('password',PasswordType::class)
+            ->add('reapedPassword', PasswordType::class)
+            ->add('status',CheckboxType::class)
+            ->getForm();*/
+
+        $usersModel = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        $usersList = [];
+        /**
+         * @var  $userModelKey
+         * @var User $userModel
+         */
+        foreach ($usersModel as $userModelKey => $userModel) {
+            $usersList[$userModelKey]['firstName'] = $userModel->getFirstName();
+            $usersList[$userModelKey]['middleName'] = $userModel->getMiddleName();
+            $usersList[$userModelKey]['lastName'] = $userModel->getLastName();
+            $usersList[$userModelKey]['userName'] = $userModel->getUserName();
+            $usersList[$userModelKey]['password'] = $userModel->getPassword();
+            $usersList[$userModelKey]['email'] = $userModel->getEmail();
+            $usersList[$userModelKey]['roll'] = $userModel->getRollId();
+            $usersList[$userModelKey]['status'] = $userModel->getStatus();
+        }
+
+        return $this->render('admin/users/add_user.html.twig', [
+            'userList' => $usersList,
+            'controller_name' => 'Users controller add user'
+            //'form' => $form->createView()
         ]);
     }
 }
